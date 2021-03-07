98422e418698c19071a3f4e64d9b30c4317a9625
alantony <alantonye@gmail.com>
2021-03-08 00:10:28 +0530

    Modified index.php- Now it just contains links to other web pages. To be converted to a proper home page later on

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-    include 'config/db.php'
+
+    require('config/def.php');
+    require('config/db.php');
+
+    //Close connection
+    mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +13,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Booking Page</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <h1>Welcome to tho booking page</h1>
+    <a href= "<?php echo ROOT_URL ?>/booking.php">Rent a vehicle</a>
+    <br>
+    <a href= "<?php echo ROOT_URL ?>/add_vehicles.php">Add a vehicle</a>
+    
 </body>
 </html>
