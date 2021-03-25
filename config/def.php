e6c47e14e81e1c5f131f29d7df86ec5b44a748be
alantony <alantonye@gmail.com>
2021-03-25 21:29:58 +0530

    adding session config file

--- config/def.php
+++ config/def.php
@@ -2,6 +2,6 @@
     define('ROOT_URL', 'http://localhost/VehicleRentalSystem/');
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', '');
+    define('DB_PASS', 'Alan@DBMS1');
     define('DB_NAME', 'VehicleRentalManagement');
 ?>
