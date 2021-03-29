0278296067bb5c2ab5f22dbba1afa3be425b5202
Sudarshan Sundarrajan <55957174+sudarshan411@users.noreply.github.com>
2021-03-29 10:01:20 +0530

    Add files via upload

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -5,3 +5,8 @@
  (6, "Motorcycle", "Yamaha RX-100", "2020", 12.5),
  (4, "Motorcycle", "Vespa", "2018", 12.5),
  (1, "Car", "Wagon R", "2020", 12.5);
+
+ insert into Vehicles values
+ ("KA 04 AB 1234", 1, 0, "Fully-Functional"),
+ ("KA 04 FC 1283", 1, 0, "Fully-Functional"),
+ ("TN 10 PL 9488", 1, 1, "AC Issues, otherwise is fine");
